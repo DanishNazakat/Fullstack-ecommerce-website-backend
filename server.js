0588af305c735408d310ad629ee44de09c929d20
DanishNazakat <danishnazakat636@gmail.com>
2026-03-09 11:13:44 +0500

    add test route

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.use(cors({
   credentials: true               // allow cookies
 }));
 
+app.get('/', (req, res)=>{
+  res.send("backend is running")
+  console.log("backend is running ")
+}
+)
 app.use('/api' , router)
 app.use('/users', userRouter)
 // app.use(authMiddleware);
